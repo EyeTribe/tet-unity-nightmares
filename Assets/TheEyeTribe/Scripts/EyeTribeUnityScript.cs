c8cf34cc56c94ee83368b9c215d8289ddf7dd6e2
Anders Bo Pedersen <anders@theeyetribe.com>
2014-12-12 09:59:41 +0100

    Initial release

---
+++ Assets/TheEyeTribe/Scripts/EyeTribeUnityScript.cs
@@ -0,0 +1,138 @@
+﻿/*
+ * Copyright (c) 2013-present, The Eye Tribe. 
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the LICENSE file in the root directory of this source tree. 
+ *
+ */
+
+using UnityEngine;
+using System.Collections;
+using TETCSharpClient;
+using TETCSharpClient.Data;
+
+namespace CompleteProject
+{
+    public class EyeTribeUnityScript : MonoBehaviour, IGazeListener
+    {
+        void Start()
+        {
+            //activate C# TET client, default port
+            GazeManager.Instance.Activate
+            (
+                GazeManager.ApiVersion.VERSION_1_0,
+                GazeManager.ClientMode.Push
+            );
+
+            //register for gaze updates
+            GazeManager.Instance.AddGazeListener(this);
+        }
+
+        public void OnGazeUpdate(GazeData gazeData)
+        {
+            //Add frame to GazeData cache handler
+            GazeDataValidator.Instance.Update(gazeData);
+        }
+
+        void Update()
+        {
+            //handle keypress
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+
+        void OnGUI()
+        {
+            int padding = 10;
+            int btnWidth = 100;
+            int btnHeight = 40;
+            int y = padding;
+
+            if (GUI.Button(new Rect(padding, y, btnWidth, btnHeight), "Press to Exit"))
+            {
+                Application.Quit();
+            }
+
+            if (!GazeManager.Instance.IsActivated)
+            {
+                y += btnHeight + padding;
+
+                if (GUI.Button(new Rect(padding, y, 200, btnHeight), "Connect To Server"))
+                {
+                    //activate C# TET client, default port
+                    GazeManager.Instance.Activate
+                    (
+                        GazeManager.ApiVersion.VERSION_1_0,
+                        GazeManager.ClientMode.Push
+                    );
+                }
+            }
+            else
+                if (!GazeManager.Instance.IsCalibrated)
+                {
+                    y += btnHeight + padding;
+                    GUI.TextArea(new Rect(padding, y, 190, 20), "EyeTribe Server not calibrated!");
+                }
+                else
+                {
+                    y += btnHeight + padding;
+
+                    string calibText;
+                    int rating;
+                    CalibrationResult result = GazeManager.Instance.LastCalibrationResult;
+                    CalibrationRatingFunction(result, out rating, out calibText);
+                    GUI.TextArea(new Rect(padding, y, 220, 20), "Calibration Result: " + calibText);
+                }
+        }
+
+        void OnApplicationQuit()
+        {
+            GazeManager.Instance.RemoveGazeListener(this);
+            GazeManager.Instance.Deactivate();
+        }
+
+        /// <summary>
+        /// Simple rating of a given calibration.
+        /// </summary>
+        /// <param name="result">Any given CalibrationResult</param>
+        /// <param name="rating">A number between 1 - 5 where 5 is the best othervise -1.</param>
+        /// <param name="strRating">A string with a rating name othervise ERROR.</param>
+        public void CalibrationRatingFunction(CalibrationResult result, out int rating, out string strRating)
+        {
+            if (result == null)
+            {
+                rating = -1;
+                strRating = "ERROR";
+                return;
+            }
+            if (result.AverageErrorDegree < 0.5)
+            {
+                rating = 5;
+                strRating = "PERFECT";
+                return;
+            }
+            if (result.AverageErrorDegree < 0.7)
+            {
+                rating = 4;
+                strRating = "GOOD";
+                return;
+            }
+            if (result.AverageErrorDegree < 1)
+            {
+                rating = 3;
+                strRating = "MODERATE";
+                return;
+            }
+            if (result.AverageErrorDegree < 1.5)
+            {
+                rating = 2;
+                strRating = "POOR";
+                return;
+            }
+            rating = 1;
+            strRating = "REDO";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,68 @@
-tet-unity-nightmares
-====================
-
 Nightmares Unity Sample for The Eye Tribe Tracker
+====
+<p>
+
+Introduction
+----
+
+This is a modified version of the [Unity 4.6](http://unity3d.com/) sample project 'Nightmares'. This variant uses eye tracking input from The Eye Tribe Tracker to add unique features to the game.
+
+![](http://theeyetribe.com/images/nightmares_1.png)
+
+The gameplay of the sample has been altered to use gaze input. 
+
+- Avatar is moved using *arrow keys* or *joystick*
+- Firing is done using *ctrl* or *joystick Fire 1*
+- Aiming is done using *eye coordinates* (instead of normal mouse position) 
+
+
+**Improvements**<br>
+In its current state, this sample does _not_ resolve all issues related to using eye input for game control.  Since eye gaze can jump from one corner of a screen to the opposite in a very short time, special input handling may be needed. The Eye Tribe encourages developers to improve the overall experience by introducing they own solutions to these issues.
+
+
+Modifications
+----
+
+The following files from the original sample have been modified. Changes to the original script files are tagged with /* @TheEyeTribe */
+
+- ./Assets/Scripts/Player/PlayerMovement.cs
+- ./Assets/Scripts/Player/PlayerShooting.cs
+<br/>
+
+The following scripts were added to the sample:
+
+- ./Assets/TheEyeTribe/EyeTribeUnityScript.cs
+- ./Assets/TheEyeTribe/GazeDataValidator.cs
+- ./Assets/TheEyeTribe/UnityGazeUtils.cs
+- ./Assets/TheEyeTribe/CalibCamera.cs
+
+The *EyeTribeUnityScript* was attached top the Main Camera of the game scene.
+
+Minor change in *Build Settings* was required to use [EyeTribe C# SDK](https://github.com/EyeTribe/tet-csharp-client). *Player Settings -> Windows -> Other Setting -> Api Compatibility Level* must be set to .NET 2.0
+
+
+Dependencies
+----
+
+This sample has been developed in Unity 4.6 and uses the [EyeTribe C# SDK](https://github.com/EyeTribe/tet-csharp-client). 
+
+
+Build
+----
+
+To build, open project in [Unity](http://unity3d.com/) and build for Windows OS or Mac OSX.
+
+Note that the EyeTribe Server currently supports Windows 7 and newer as well as Mac OSX 10.8 and never. Support for other platforms will be added in the future.
+
+
+FAQ
+----
+
+Should question arise, do not hesitate to post them on [The Eye Tribe Forum](http://theeyetribe.com/forum/).
+
+
+Changelog
+----
+
+0.9.49 (2014-12-12)
+- Initial release
